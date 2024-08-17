c20b19177a819f23e2b1f776f8b6f8ac7726c335
Sakib928 <sakibgscian@gmail.com>
2024-08-17 10:29:01 +0600

    added pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,10 +1,22 @@
+import useAuth from "../hooks/useAuth";
+
 const Pagination = () => {
+  const { pages, currentPage, setCurrentPage } = useAuth();
+  console.log(pages[pages.length - 1] + 1);
+  const handleNext = () => {
+    setCurrentPage(currentPage + 1);
+  };
+  const handlePrev = () => {
+    setCurrentPage(currentPage - 1);
+  };
   return (
-    <div className="flex justify-center space-x-1 dark:text-gray-800 mt-4">
+    <div className="flex justify-center space-x-1 dark:text-gray-800 my-4 font-bold">
       <button
+        onClick={handlePrev}
         title="previous"
         type="button"
         className="inline-flex items-center justify-center w-8 h-8 py-0 border rounded-md shadow-md dark:bg-gray-50 dark:border-gray-100"
+        disabled={currentPage == 1}
       >
         <svg
           viewBox="0 0 24 24"
@@ -18,38 +30,28 @@ const Pagination = () => {
           <polyline points="15 18 9 12 15 6"></polyline>
         </svg>
       </button>
+      {pages.map((page) => {
+        return (
+          <button
+            onClick={() => setCurrentPage(page + 1)}
+            key={page}
+            type="button"
+            title="Page 1"
+            className={`inline-flex items-center justify-center w-8 h-8 text-sm font-semibold border rounded shadow-md dark:bg-gray-50 dark:text-default-600 dark:border-default-600 ${
+              currentPage == page + 1 ? "scale-125 text-blue-600" : "scale-100"
+            }`}
+          >
+            {page + 1}
+          </button>
+        );
+      })}
+
       <button
-        type="button"
-        title="Page 1"
-        className="inline-flex items-center justify-center w-8 h-8 text-sm font-semibold border rounded shadow-md dark:bg-gray-50 dark:text-default-600 dark:border-default-600"
-      >
-        1
-      </button>
-      <button
-        type="button"
-        className="inline-flex items-center justify-center w-8 h-8 text-sm border rounded shadow-md dark:bg-gray-50 dark:border-gray-100"
-        title="Page 2"
-      >
-        2
-      </button>
-      <button
-        type="button"
-        className="inline-flex items-center justify-center w-8 h-8 text-sm border rounded shadow-md dark:bg-gray-50 dark:border-gray-100"
-        title="Page 3"
-      >
-        3
-      </button>
-      <button
-        type="button"
-        className="inline-flex items-center justify-center w-8 h-8 text-sm border rounded shadow-md dark:bg-gray-50 dark:border-gray-100"
-        title="Page 4"
-      >
-        4
-      </button>
-      <button
+        onClick={handleNext}
         title="next"
         type="button"
         className="inline-flex items-center justify-center w-8 h-8 py-0 border rounded-md shadow-md dark:bg-gray-50 dark:border-gray-100"
+        disabled={currentPage == pages[pages.length - 1] + 1}
       >
         <svg
           viewBox="0 0 24 24"
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -10,17 +10,31 @@ const AuthProvider = ({ children }) => {
   const [searchText, setSearchText] = useState("");
   const [brand, setBrand] = useState("");
   const [category, setCategory] = useState("");
+  const itemsPerPage = 6;
+  const [count, setCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const numberOfPages = Math.ceil(count / itemsPerPage);
+  const pages = [...Array(numberOfPages).keys()];
 
   useEffect(() => {
     axios
       .get(
-        `http://localhost:5000/products/?searchText=${searchText}&brand=${brand}&category=${category}`
+        `http://localhost:5000/products/?searchText=${searchText}&brand=${brand}&category=${category}&page=${currentPage}`
       )
       .then((res) => {
         // console.log(res?.data);
         setShowProducts(res?.data);
       });
-  }, [searchText, brand, category]);
+  }, [searchText, brand, category, currentPage]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/productsCount")
+
+      .then((res) => {
+        setCount(res.data.count);
+      });
+  }, []);
 
   const authInfo = {
     showProducts,
@@ -32,6 +46,9 @@ const AuthProvider = ({ children }) => {
     setSearchText,
     setBrand,
     setCategory,
+    pages,
+    currentPage,
+    setCurrentPage,
   };
   return (
     <authContext.Provider value={authInfo}>{children}</authContext.Provider>
